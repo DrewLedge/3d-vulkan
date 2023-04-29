0c8bae6d6bebebe101177ba2a6845b31558d6ffa
Drew <83474944+drewuw@users.noreply.github.com>
2023-04-29 17:06:26 -0400

    vulkan setup
    Because windows GDI doesn't use the GPU, I'm deciding to switch over to Vulkan and GLFW.

--- 3d/3d/3d.cpp
+++ 3d/3d/3d.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <cmath>
 #include <random>
+
 double PI = 3.14159265358979323846;
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 typedef struct {
