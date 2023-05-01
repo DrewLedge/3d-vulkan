f0bc52d320d1482c811a42b6cfef04148f3fa8ae
Drew <83474944+drewuw@users.noreply.github.com>
2023-05-01 09:45:11 -0400

    more commented code for better remembering

--- vulkan-3d/vulkan-3d/vulkan-3d.cpp
+++ vulkan-3d/vulkan-3d/vulkan-3d.cpp
@@ -24,15 +24,15 @@ private:
 	void initWindow() {
 		glfwInit();
 
-		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // GLFW_NO_API tells GLFW to not create an OpenGL context but rather to only create a window
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 	}
-	void createInstance() {
+	void createInstance() { //this fucntion creates an instance of the Vulkan library
 		VkApplicationInfo appInfo{};
-		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO; // VK_STRUCTURE_TYPE_APPLICATION_INFO is a constant that tells Vulkan which structure you are using, which allows the implementation to read the data accordingly
+		appInfo.pApplicationName = "Hello Triangle"; //the "p" is a naming convention that indicates a pointer to a null-terminated string
 		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 		appInfo.pEngineName = "No Engine"; //an example would be Unreal, Unity or Godot
 		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
@@ -56,11 +56,11 @@ private:
 	}
 	void mainLoop() {
 		while (!glfwWindowShouldClose(window)) { // while window is not closed
-			glfwPollEvents();
+			glfwPollEvents(); // this function checks if any events are triggered (like keyboard input or mouse movement events), updates the window state, and calls the corresponding functions (which we can set via callback methods)`
 		}
 	}
 	void cleanup() {
-		vkDestroyInstance(instance, nullptr);
+		vkDestroyInstance(instance, nullptr); //equivelant of WM_DESTROY in windowsGDI
 
 		glfwDestroyWindow(window);
 		glfwTerminate();
